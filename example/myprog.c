697118716d4fb5aea95289a8199ee4287f5ac602
Sam Vervaeck <sam@vervaeck.be>
2017-02-25 14:14:45 +0100

    Make myprog in example a little more complex

--- example/myprog.c
+++ example/myprog.c
@@ -3,5 +3,14 @@ float add(float a, float b) {
   return a + b;
 }
 
+typedef struct Point {
+  int x;
+  int y;
+} Point;
+
+void point_add_to(Point a, Point b) {
+  a.x += b.x;
+  a.y += b.y;
+}
 
 
